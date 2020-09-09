a935e69110ade1c5d774090c12ea1c05a7d35268
Christy Hauschild <christyhauschid@gmail.com>
2020-09-09 20:26:06 -0300

    feat: setup global styles and scripts

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
-const App: React.FC = () => {
-  return <div />;
-};
+import GlobalStyle from './styles/global';
+
+const App: React.FC = () => (
+  <>
+    <div />
+    <GlobalStyle />
+  </>
+);
 
 export default App;
