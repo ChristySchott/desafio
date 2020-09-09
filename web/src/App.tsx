4a4dc2956a05ca8ea605ccfde2ad1693912769d4
Christy Hauschild <christyhauschid@gmail.com>
2020-09-09 20:32:16 -0300

    feat: configure routes

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
+import Routes from './routes';
 import GlobalStyle from './styles/global';
 
 const App: React.FC = () => (
   <>
-    <div />
+    <Routes />
     <GlobalStyle />
   </>
 );
---
+++ web/src/routes/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Dashboard from '../pages/Dashboard';
+
+const Routes: React.FC = () => (
+  <Router>
+    <Switch>
+      <Route path="/" exact component={Dashboard} />
+    </Switch>
+  </Router>
+);
+
+export default Routes;
