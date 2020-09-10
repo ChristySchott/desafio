1e9a497f53d7c3627344e927e736413b1775e33a
Christy Hauschild <christyhauschid@gmail.com>
2020-09-10 11:20:54 -0300

    feat: create an api service

--- web/server.js
+++ web/server.js
@@ -6,15 +6,15 @@ app.use('/api', serveStatic('mock-api', {
   'index': false,
   'setHeaders': setJsonHeaders
 }))
- 
+
 // Set header to force download
-function setJsonHeaders (res, path) {
+function setJsonHeaders(res, path) {
   res.setHeader('Content-type', 'application/json')
 }
 
-// Serve up public folder
-app.use('/', serveStatic('public', {'index': ['index.html', 'index.htm']}))
+// Serve up build folder
+app.use('/', serveStatic('build', { 'index': ['index.html'] }))
 
-app.listen(8888, function() {
-    console.log('Acesse: http://localhost:8888')
+app.listen(3000, function () {
+  console.log('Acesse: http://localhost:3000')
 });
