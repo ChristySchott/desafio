6aadec7344b17e9520b162235384ae68b002e7f1
Christy Hauschild <christyhauschid@gmail.com>
2020-09-10 10:13:12 -0300

    feat: responsive main display on huge screen

--- web/src/pages/Dashboard/styles.ts
+++ web/src/pages/Dashboard/styles.ts
@@ -4,12 +4,18 @@ const Main = styled.div`
   display: flex;
   align-items: center;
   flex-direction: column;
+  max-width: 1048px;
+  margin: 0 auto;
 
   @media (min-width: 768px) {
     align-items: flex-start;
     flex-direction: row;
     margin-top: 20px;
     padding: 0 15px;
+  }
+
+  @media (min-width: 1440px) {
+    padding: 0;
   }
 `;
 
