5345fc5f6f3e7c471aab669f9282b9c493f402ba
Christy Hauschild <christyhauschid@gmail.com>
2020-09-10 21:22:45 -0300

    feat: create filter hooks

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 
 import Routes from './routes';
 import GlobalStyle from './styles/global';
+import AppProvider from './hooks';
 
 const App: React.FC = () => (
-  <>
+  <AppProvider>
     <Routes />
     <GlobalStyle />
-  </>
+  </AppProvider>
 );
 
 export default App;
---
+++ web/src/hooks/filter.tsx
@@ -0,0 +1,35 @@
+import React, { createContext, useContext, useState } from 'react';
+
+interface FilterContextData {
+  filter: string;
+  setFilter: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const FilterContext = createContext<FilterContextData>({} as FilterContextData);
+
+const FilterProvider: React.FC = ({ children }) => {
+  const [filter, setFilter] = useState('tenis');
+
+  return (
+    <FilterContext.Provider
+      value={{
+        filter,
+        setFilter,
+      }}
+    >
+      {children}
+    </FilterContext.Provider>
+  );
+};
+
+function useFilter(): FilterContextData {
+  const context = useContext(FilterContext);
+
+  if (!context) {
+    throw new Error('useFilter must be used within an FilterContext');
+  }
+
+  return context;
+}
+
+export { FilterProvider, useFilter };
